764f708a820b7c2aed66a73923eba20a1791558b
sumansourabh9155 <sumansourabh9155@gmail.com>
2022-05-31 15:25:15 +0530

    Started adding Mobile Responsive in Navbar

--- src/component/UI/Navbar/Navbar.jsx
+++ src/component/UI/Navbar/Navbar.jsx
@@ -1,9 +1,15 @@
-import { AiOutlineMenuFold, AiOutlineMenuUnfold } from 'react-icons/ai';
+import { BiMenuAltRight } from 'react-icons/bi';
+import { ImCross } from 'react-icons/im';
+import { useState } from 'react';
 
 const Navbar = () => {
+    const [showMenu, setShowMenu] = useState(false);
+    const setMenu = () => {
+        setShowMenu(!showMenu);
+    }
     return (<div>
 
-        <div className="flex items-center justify-between h-24 max-w-[1240p] mx-auto p-4" >
+        <div className="flex items-center justify-between h-24 max-w-[1240p] mx-auto p-4 text-white" >
             <h1 className="w-full text-3xl font-bold">Suman <spam className="text-[#3DF5EA]">Sourabh</spam></h1>
             <ul className="flex hidden">
                 <li className="p-4">Home</li>
@@ -11,15 +17,19 @@ const Navbar = () => {
                 <li className="p-4">Projects</li>
                 <li className="p-4">Contact Me</li>
             </ul>
-            <div>
-                <AiOutlineMenuFold size={25} />
+            <div onClick={setMenu}>
+                {!showMenu ? <ImCross size={25} /> : <BiMenuAltRight size={25} />}
             </div>
-            <ul className="fixed left-0 top-0 w-[60%] h-full border-r-grey-900">
-                <li className="p-4">Home</li>
-                <li className="p-4">About</li>
-                <li className="p-4">Projects</li>
-                <li className="p-4">Contact Me</li>
-            </ul>
+            <div className={!showMenu ? "fixed left-0 top-0 w-[60%] h-full border-r-grey-900 pt-8  border-b-2 bg-gray-800 ease-in-out duration-500" : "fixed left-[-100%]"}>
+                <h1 className="w-full text-3xl font-bold pl-8">Suman <spam className="text-[#3DF5EA]">Sourabh</spam></h1>
+
+                <ul className='p-4 uppercase'>
+                    <li className="p-4 border-b border-grey-800">Home</li>
+                    <li className="p-4 border-b border-grey-800">About</li>
+                    <li className="p-4 border-b border-grey-800">Projects</li>
+                    <li className="p-4">Contact Me</li>
+                </ul>
+            </div>
         </div>
 
     </div >)
