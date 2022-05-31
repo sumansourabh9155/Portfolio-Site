2b04de489b6fed260897d62b1c96df2abdb22c78
sumansourabh9155 <sumansourabh9155@gmail.com>
2022-05-31 15:41:07 +0530

    Navbar Completed

--- src/component/UI/Navbar/Navbar.jsx
+++ src/component/UI/Navbar/Navbar.jsx
@@ -10,14 +10,15 @@ const Navbar = () => {
     return (<div>
 
         <div className="flex items-center justify-between h-24 max-w-[1240p] mx-auto p-4 text-white" >
-            <h1 className="w-full text-3xl font-bold">Suman <spam className="text-[#3DF5EA]">Sourabh</spam></h1>
-            <ul className="flex hidden">
+            <h1 className="text-3xl font-bold">Suman <spam className="text-[#3DF5EA]">Sourabh</spam></h1>
+            <ul className="hidden md:flex">
                 <li className="p-4">Home</li>
                 <li className="p-4">About</li>
                 <li className="p-4">Projects</li>
                 <li className="p-4">Contact Me</li>
             </ul>
-            <div onClick={setMenu}>
+
+            <div onClick={setMenu} className="block md:hidden">
                 {!showMenu ? <ImCross size={25} /> : <BiMenuAltRight size={25} />}
             </div>
             <div className={!showMenu ? "fixed left-0 top-0 w-[60%] h-full border-r-grey-900 pt-8  border-b-2 bg-gray-800 ease-in-out duration-500" : "fixed left-[-100%]"}>
